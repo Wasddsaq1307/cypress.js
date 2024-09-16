166742dc5764291932f428c76318cbb8f7d1a813
Macsim <Kilcam2048@yandex.ru>
2024-09-16 23:41:38 +0300

    Добавил автотесты

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,65 @@
+describe('Проверка авторизации', function () {
+
+    it('Верный пароль и верный логин', function () {
+         cy.visit('https://login.qa.studio');
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+         cy.get('#mail').type('USER_LOGIN');
+         cy.get('#pass').type('USER_PASSWORD');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+ 
+     it('Верный логин и неверный пароль', function () {
+         cy.visit('https://login.qa.studio');
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+         cy.get('#mail').type('USER_LOGIN');
+         cy.get('#pass').type('USER_PASSWORD');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#messageHeader').contains('Такого логина или пароля нет');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+ 
+     it('Валидация на наличие @', function () {
+         cy.visit('https://login.qa.studio');
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+         cy.get('#mail').type('USER_LOGIN');
+         cy.get('#pass').type('USER_PASSWORD');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+     it('Восстановление пароля', function () {
+         cy.visit('https://login.qa.studio');
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+         cy.get('#forgotEmailButton').click();
+         cy.get('#mailForgot').type('USER_LOGIN');
+         cy.get('#restoreEmailButton').click();
+         cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+     it('Неверный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('USER_LOGIN');
+        cy.get('#pass').type('USER_PASSWORD');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+     it('Приведение к строчным буквам в логине', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type(' USER_LOGIN');
+        cy.get('#pass').type('USER_PASSWORD');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+     
+ })
